a6761d71c1a53fec86fbe9af3b33aaf5d90fd00a
Rahaf-Armanazi <armnazyrwrw@gmail.com>
2024-09-19 17:12:01 +0300

    wave line

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Home from './pages/Home/Home'; // استيراد المكون
 export default function App() {
-    console.log(",fdnvfdngjk");
-}
+
+  return (
+    <div>
+      {/* <h1>رسم دالة الجيب باستخدام React</h1> */}
+      <Home />
+    </div>
+  );
+};
