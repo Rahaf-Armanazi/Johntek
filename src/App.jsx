a0fb505cf6613dcd8b192d9b610c288d6347610a
Sabihakh <Totymaro999666333@gmail.com>
2024-09-26 17:23:20 +0300

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import DashBoard from "./pages/DashBoard/DashBoard";
+ 
 export default function App() {
-    console.log("Sabiha");
+    return (
+        <div >
+          {/* <h1>رسم دالة الجيب باستخدام React</h1> */}
+          <DashBoard/>
+          <DashBoard/><DashBoard/><DashBoard/>
+        </div>
+      );
 }
