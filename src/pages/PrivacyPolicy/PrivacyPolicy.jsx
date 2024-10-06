0ef5daa663820e68fab8c4722862d9ebaa1a16d9
Sabihakh <Totymaro999666333@gmail.com>
2024-10-06 14:41:09 +0300

    build

---
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./PrivacyPolicy.css";
+
+function PrivacyPolicy1() {
+  return (
+    <div className="privacy">
+      <h1 className="text5" id="H1privacy">
+        {" "}
+        Privacy Policy{" "}
+      </h1>
+      <h6 className="text5"> Last updated October 3, 2024 </h6>
+      <p className="text5">
+        {" "}
+        At JohntekValves.com, your privacy is very important to us. This privacy
+        policy document outlines the types of information we do not collect when
+        you use our website, as well as how we ensure your privacy.
+      </p>
+      <p className="text5">
+        {" "}
+        <span className="span1">Information We Do Not Collect </span> We want to
+        assure you that:
+      </p>
+      <p className="text5">
+        No personal data is collected when you visit or interact with our
+        website. We do not ask for or store any personally identifiable
+        information (PII), such as your name, email address, or phone number. We
+        do not collect cookies, IP addresses, or any form of analytics or
+        tracking data.</p>
+        <p><span className="span1">Third-Party Services :</span>
+        Since we do not collect any personal information, no third-party
+        services are used to track or monitor user activity on this site. Any
+        external links on our website may lead to third-party websites, and we
+        recommend you review their privacy policies separately as we are not
+        responsible for their practices.
+      </p>
+      <p className="text5">
+        <span className="span1"> Data Security :</span>
+        As we do not collect any personal data, there are no security risks
+        associated with personal information. However, we still prioritize
+        website security to ensure a safe browsing experience for all users.
+      </p>
+      <p className="text5">
+        <span className="span1">Changes to This Privacy Policy :</span>
+        We may update this privacy policy periodically to reflect any changes in
+        our practices or legal obligations. The latest version will always be
+        available on this page, with the effective date listed at the top.
+      </p>{" "}
+      <p className="text5">
+        <span className="span1">Contact Us :</span>If you have any questions or
+        concerns about this privacy policy, please feel free to contact us at :
+       <a href="https://github.com/Sabihakh"> info@johntekvalves.com</a>{" "}
+      </p>
+    </div>
+  );
+}
+export default PrivacyPolicy1;
